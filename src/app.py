49d4721c56d31e56762e5747b113094b5ac4aafd
PakDanan <danan_p@yahoo.com>
2023-08-12 21:05:02 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import cv2
 from PIL import Image
 import numpy as np
-import os
+#import os
 
 from const import CLASSES, COLORS
 from settings import DEFAULT_CONFIDENCE_THRESHOLD, DEMO_IMAGE, MODEL, PROTOTXT
